875c45ff9d06447ccd55495ae7f495ea5e552bbe
Nick Reilingh <nreiling@bard.edu>
2021-04-05 15:27:47 -0400

    this all works! shellspec is awesome

--- spec/schema_spec.sh
+++ spec/schema_spec.sh
@@ -1,7 +1,24 @@
-Describe 'hello.sh'
-  Include lib/hello.sh
-  It 'says hello'
-    When call hello ShellSpec
-    The output should equal 'Hello ShellSpec!'
+Describe 'schema.cue'
+  Describe 'model validation'
+    Parameters:dynamic
+      for model in spec/examples/*; do
+        %data $model
+      done
+    End
+    Specify "$1 is valid"
+      When run command cue vet $1 schema.cue -c -v
+      The status should equal 0
+      The stdout should be blank
+      The stderr should be blank
+    End
+  End
+
+  Describe 'self validation'
+    Specify 'repo.cue is valid'
+      When run command cue vet repo.cue schema.cue -c -v
+      The status should equal 0
+      The stdout should be blank
+      The stderr should be blank
+    End
   End
 End
