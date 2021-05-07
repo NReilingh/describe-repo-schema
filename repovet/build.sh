c68b31e778002d060e59db546d06f5de13065e70
Nick Reilingh <nreiling@bard.edu>
2021-05-06 23:48:02 -0400

    refactor repovet using support directory

--- repovet/build.sh
+++ repovet/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-mkdir -p dist > /dev/null
-cp -r ../schema dist/
+mkdir -p dist/support > /dev/null
+cp -r ../schema dist/support/
 cp repovet dist/
-cp ../spec/integration/model-example.cue dist/
+cp ../spec/integration/model-example.cue dist/support
